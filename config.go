f66299c1eb435e7c6ab2761a9c825a657b7ac8c4
jevjay <j.v.jermakov@gmail.com>
2021-01-18 21:56:09 -0400

    Update command config file according to new structure
    This is the latest version of the config file structure (alpha-v0.1)
    It provides a bare minimum for the configuration of the command

--- config.go
+++ config.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+
+	"gopkg.in/yaml.v2"
 )
 
-// CommandsConfig stores command data list
-type CommandsConfig struct {
-	Command []Command `json:"command" yaml:"command"`
+// Commands contains a list of commands configs
+type Commands struct {
+	Configs CommandConfig `json:"config" yaml:"config"`
 }
 
-// Command stores configuration of a command retrieved from a config file
-type Command struct {
-	Answer string `json:"answer" yaml:"answer"`
-	Name   string `json:"name" yaml:"name"`
-	Desc   string `json:"desc" yaml:"desc"`
-	Image  string `json:"image" yaml:"image"`
+// CommandConfig stores command input/output configurations
+type CommandConfig struct {
+	Input []InputConfig `json:"input" yaml:"input"`
+}
+
+// InputConfig stores configuration of a command input
+type InputConfig struct {
+	Args    string       `json:"args" yaml:"args"`
+	Command string       `json:"command" yaml:"command"`
+	Desc    string       `json:"desc" yaml:"desc"`
+	Error   string       `json:"error" yaml:"error"`
+	Image   string       `json:"image" yaml:"image"`
+	Type    string       `json:"type" yaml:"type"`
+	Timeout string       `json:"timeout" yaml:"timeout"`
+	Output  OutputConfig `json:"output" yaml:"output"`
+}
+
+// OutputConfig stores configuration of a command output
+type OutputConfig struct {
+	Message    string `json:"message" yaml:"message"`
+	Attachment string `json:"attachment" yaml:"attachment"`
+	Color      string `json:"color" yaml:"color"`
 }
 
 // ParseConfig parses configuration files command(s)
-func (c *CommandsConfig) ParseConfig(path string) error {
+func (c *Commands) ParseConfig(path string, debug bool) error {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatalf("[ERROR] Failed to parse command config file: %s", err)
@@ -31,5 +48,10 @@ func (c *CommandsConfig) ParseConfig(path string) error {
 		log.Fatalf("[ERROR] Failed to parse command config file: %s", err)
 		return err
 	}
+	// Debug output
+	if debug {
+		log.Println("Commands config: \n", string(data))
+	}
+
 	return nil
 }
